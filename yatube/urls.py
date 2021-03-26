e0d58ade54f161700b3b9204e38230d9c3b24f96
Alexandr <s2dent18@yandex.ru>
2021-03-26 18:18:33 +0500

    Ver 1.0

--- yatube/urls.py
+++ yatube/urls.py
@@ -6,7 +6,6 @@ from django.urls import include, path
 
 handler404 = "posts.views.page_not_found"  # noqa
 handler500 = "posts.views.server_error"  # noqa
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("about/", include("about.urls", namespace="about")),
